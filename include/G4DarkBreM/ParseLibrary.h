a908afb2fe2878bcd80ab996342c81af9292127a
tomeichlersmith <eichl008@umn.edu>
2023-05-17 15:39:31 -0500

    copy over updated documentation to front header

--- include/G4DarkBreM/ParseLibrary.h
+++ include/G4DarkBreM/ParseLibrary.h
@@ -42,17 +42,18 @@ struct OutgoingKinematics {
  * names the columns. These column names have no requirements
  * (besides the existence of this line).
  *
- * The CSV is required to have 9 columns on all non-empty lines of the file.
- * The 9 columns of the CSV all are in MeV and _in order_ are
- * 1. The incident lepton energy
- * 2. The total energy of the recoil
- * 3. The x-component of the recoil momentum
- * 4. The y-component of the recoil momentum
- * 5. The z-component of the recoil momentum
- * 6. The total energy of the A'
- * 7. The x-component of the A' momentum
- * 8. The y-component of the A' momentum
- * 9. The z-component of the A' momentum
+ * The CSV is required to have 10 columns on all non-empty lines of the file.
+ * The 10 columns of the CSV all are in MeV and _in order_ are
+ * 1. The target Z
+ * 2. The incident lepton energy
+ * 3. The total energy of the recoil
+ * 4. The x-component of the recoil momentum
+ * 5. The y-component of the recoil momentum
+ * 6. The z-component of the recoil momentum
+ * 7. The total energy of the A'
+ * 8. The x-component of the A' momentum
+ * 9. The y-component of the A' momentum
+ * 10. The z-component of the A' momentum
  *
  * ### LHE
  * The LHE files must have dark brem events in it where "dark brem event" 
@@ -66,6 +67,16 @@ struct OutgoingKinematics {
  *   <skip-line>
  *   aprime_id 1 <skip> <skip> <skip> <skip> px py pz E m
  * ```
+ *
+ * We also check each line if it contains a 'Znuc' string. Looking
+ * for the target Z value from this line matching the following format.
+ * ```
+ *     <num>   <Z> # Znuc <other comments>
+ * ```
+ * This is supposed to match a line of the param_card dumped into the header
+ * of the LHE. In other words, this parser should function properly if your
+ * MG/ME model has a configurable parameter in param_card.dat for the target
+ * Z and that parameter is called 'Znuc'.
  *
  * This matches a subcomponent of the LHE scheme written by MadGraph/MadEvent
  * (hence the reason this is the "lhe" parser); however, a lot of information
