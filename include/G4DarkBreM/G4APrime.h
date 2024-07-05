ad588b98bc0fd1637b7496ec14749158a5c25a8f
Rob Mina <rob.mina@charchrist.com>
2024-07-04 23:57:02 -0400

    Option to scale dark photon and option to decay dark photon

--- include/G4DarkBreM/G4APrime.h
+++ include/G4DarkBreM/G4APrime.h
@@ -27,7 +27,70 @@ class G4DecayTable;
  * ConstructParticle function of a physics constructor.
  */
 class G4APrime : public G4ParticleDefinition {
- private:
+ public:
+  /**
+   * @enum DecayMode
+   *
+   * How to handle APrime decays.
+   */
+  enum class DecayMode {
+    /**
+     * No decay/stable -- this is the default.
+     */
+    NoDecay = 1,
+
+    /**
+     * Flat decay -- the decay proper time will be randomly sampled
+     * from a uniform distribution whose maximum and minimum are set
+     * in the G4DarkBreMModel::GenerateChange() method based on parameters
+     * of that class.
+     *
+     * As with GeantDecay, the APrime will decay to e-/e+.
+     */
+     FlatDecay = 2,
+
+     /**
+      * Let Geant4 handle the decay. In this case, the lifetime
+      * must be included as a parameter of the constructor.
+      *
+      * e-/e+ is the only decay channel.
+      */
+      GeantDecay = 3
+  };
+
+  /**
+   * Accessor for APrime definition
+   *
+   * @throws std::runtime_error if the APrime is not initialized yet.
+   *
+   * @see Initialize for configuring and constructing the APrime
+   * at the start of a run.
+   */
+  static G4APrime* APrime();
+
+  /**
+   * Initialize the APrime particle with the passed configuration
+   *
+   * @throws std::runtime_error if the APrime has already been initialized
+   *
+   * @param[in] mass The mass of the APrime in MeV
+   * @param[in] id The PDG ID number to use for the APrime particle
+   * @param[in] tau The proper lifetime 
+   *                (only used if decay_mode is set to GeantDecay)
+   * @param[in] decay_mode G4APrime::DecayMode on whether/how to decay the A'
+   *
+   * The default value for the PDG ID is set to 62 and has been arbitrarily
+   * chosen from the range defined by 11(c) in the
+   * [PDG ID numbering
+   * scheme](https://pdg.lbl.gov/2007/reviews/montecarlorpp.pdf), avoiding the
+   * already-defined "one-of-a-kind" particles using 39, 41, and 42.
+   */
+  static void Initialize(double mass, int id = 62, double tau = -1.0, 
+                         DecayMode decay_mode = DecayMode::NoDecay);
+
+  static DecayMode getDecayMode() { return decay_mode_; }
+
+  private:
   /** Reference to single particle definition of A' */
   static G4APrime* theAPrime;
 
@@ -47,39 +110,14 @@ class G4APrime : public G4ParticleDefinition {
                              lepton, baryon, encoding, stable, lifetime,
                              decaytable) {}
 
+  static DecayMode decay_mode_;
+
   /**
    * Destructor
    *
    * Does nothing on purpose.
    */
   virtual ~G4APrime() {}
-
- public:
-  /**
-   * Accessor for APrime definition
-   *
-   * @throws std::runtime_error if the APrime is not initialized yet.
-   *
-   * @see Initialize for configuring and constructing the APrime
-   * at the start of a run.
-   */
-  static G4APrime* APrime();
-
-  /**
-   * Initialize the APrime particle with the passed configuration
-   *
-   * @throws std::runtime_error if the APrime has already been initialized
-   *
-   * @param[in] mass The mass of the APrime in MeV
-   * @param[in] id The PDG ID number to use for the APrime particle
-   *
-   * The default value for the PDG ID is set to 62 and has been arbitrarily
-   * chosen from the range defined by 11(c) in the
-   * [PDG ID numbering
-   * scheme](https://pdg.lbl.gov/2007/reviews/montecarlorpp.pdf), avoiding the
-   * already-defined "one-of-a-kind" particles using 39, 41, and 42.
-   */
-  static void Initialize(double mass, int id = 62);
 };
 
 #endif  // SIMCORE_DARKBREM_G4APRIME_H_
--- src/G4DarkBreM/G4APrime.cxx
+++ src/G4DarkBreM/G4APrime.cxx
@@ -8,9 +8,13 @@
 
 #include "G4ParticleTable.hh"
 #include "G4PhysicalConstants.hh"
+#include "G4DecayTable.hh"
+#include "G4PhaseSpaceDecayChannel.hh"
 #include "globals.hh"
 
 G4APrime* G4APrime::theAPrime = 0;
+
+G4APrime::DecayMode G4APrime::decay_mode_ = G4APrime::DecayMode::NoDecay;
 
 G4APrime* G4APrime::APrime() {
   if (!theAPrime) {
@@ -22,10 +26,13 @@ G4APrime* G4APrime::APrime() {
   return theAPrime;
 }
 
-void G4APrime::Initialize(double mass, int id) {
+void G4APrime::Initialize(double mass, int id, double tau,
+                          G4APrime::DecayMode decay_mode) {
   if (theAPrime)
     throw std::runtime_error(
         "Attempting to initialize the APrime particle more than once.");
+
+  G4APrime::decay_mode_ = decay_mode;
 
   /**
    * Here are the properties of the formal Geant4 dark photon we define.
@@ -46,16 +53,31 @@ void G4APrime::Initialize(double mass, int id) {
    * lepton number | 0
    * baryon number | 0
    * PDG ID encoding | **configured**
-   * is stable (no decay) | true
-   * lifetime | -1 (i.e. no decay)
-   * decay table | nullptr (i.e. no decay)
+   * is stable (no decay) | depends on DecayMode
+   * lifetime | depends on DecayMode
+   * decay table | depends on DecayMode
    */
-  theAPrime = new G4APrime(
+  if (decay_mode == G4APrime::DecayMode::GeantDecay) {
+    G4DecayTable* table = new G4DecayTable();
+    G4VDecayChannel* mode = new G4PhaseSpaceDecayChannel("A^1", 1.0, 2, 
+                                                         "e-", "e+");
+    table->Insert(mode);
+    theAPrime = new G4APrime(
+      "A^1" /* short name */, mass * MeV, 0. /* mass width */,
+      0. /*electric charge */, 0 /* spin */, 0 /* parity */,
+      0 /* conjugation */, 0 /* isospine */, 0 /* isospin3 */, 0 /* G parity */,
+      "APrime" /* long name */, 0 /* lepton number */, 0 /* baryon number */,
+      id, false /* stable? */, tau /* lifetime */, table /* decay table */
+    );
+  } else {
+    theAPrime = new G4APrime(
       "A^1" /* short name */, mass * MeV, 0. /* mass width */,
       0. /*electric charge */, 0 /* spin */, 0 /* parity */,
       0 /* conjugation */, 0 /* isospine */, 0 /* isospin3 */, 0 /* G parity */,
       "APrime" /* long name */, 0 /* lepton number */, 0 /* baryon number */,
       id, true /* stable? */, -1 /* lifetime */, nullptr /* decay table */
-  );
+    );
+  }
+
   return;
 }
